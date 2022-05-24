1dcfab9956a02d5deac6148cc05cd21b5100c3c2
taleshrocha <taleshrochaz@gmail.com>
2022-05-23 21:54:25 -0300

    ws done

--- unity-2/mux-demux-codif-decod/exercise-1/ws/demux.vhdl
+++ unity-2/mux-demux-codif-decod/exercise-1/ws/demux.vhdl
@@ -3,21 +3,24 @@ use ieee.std_logic_1164.all;
 
 entity demux is
   port(
-  f : in std_logic_vector (1 downto 0);
-  s: in std_logic_vector (1 downto 0);
+        f : in std_logic_vector (1 downto 0);
+        s: in std_logic_vector (1 downto 0);
   a, b, c, d: out std_logic_vector (1 downto 0)
 );
 end demux;
 
 architecture behaviour of demux is
 begin
-  process (f,s) is
-  begin
-    with s select
-    a <= f when "00";
+  with s select
+    a <= f when "00",
+         "00" when others;
+  with s select
     b <= f when "01",
+         "00" when others;
+  with s select
     c <= f when "10",
-    d <= f when others;
-
-  end process;
+         "00" when others;
+  with s select
+    d <= f when "11",
+         "00" when others;
 end behaviour;
