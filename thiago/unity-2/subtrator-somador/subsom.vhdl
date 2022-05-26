aaa34d71c0f8b302aab2832722f65ef89a29896f
ThiagoOliveiraCordeiro <thiagoty52@gmail.com>
2022-05-26 11:08:06 -0300

    finished subtrator and somador - thiago version

--- thiago/unity-2/subtrator-somador/subsom.vhdl
+++ thiago/unity-2/subtrator-somador/subsom.vhdl
@@ -18,15 +18,24 @@ architecture main of subsom is
 );
   end component;
   signal carry : std_logic_vector(7 downto 0);
+  signal aux : std_logic;
   
 begin
-  som0: somcom port map (a => a(0), b => (b(0) xor s), cin => s, s => f(0), cout => carry());
-  som1: somcom port map (a(1), b(1) xor s, carry, f(1), carry);
-  som2: somcom port map (a(2), b(2) xor s, carry, f(2), carry);
-  som3: somcom port map (a(3), b(3) xor s, carry, f(3), carry);
-  som4: somcom port map (a(4), b(4) xor s, carry, f(4), carry);
-  som5: somcom port map (a(5), b(5) xor s, carry, f(5), carry);
-  som6: somcom port map (a(6), b(6) xor s, carry, f(6), carry);
-  som7: somcom port map (a(7), b(7) xor s, carry, f(7), carry);
-  aux <= b(0) xor s; som0: somcom port map (a => a(0), b => aux, cin => s, s => f(0), cout => carry(0));
+  aux <= b(0) xor s; 
+  som0: somcom port map (a => a(0), b => aux, cin => s, s => f(0), cout => carry(0));
+  aux <= b(1) xor s;
+  som1: somcom port map (a => a(1), b => aux, cin => s, s => f(1), cout => carry(1));
+  aux <= b(2) xor s;
+  som2: somcom port map (a => a(2), b => aux, cin => s, s => f(2), cout => carry(2));
+  aux <= b(3) xor s;
+  som3: somcom port map (a => a(3), b => aux, cin => s, s => f(3), cout => carry(3));
+  aux <= b(4) xor s;
+  som4: somcom port map (a => a(4), b => aux, cin => s, s => f(4), cout => carry(4));
+  aux <= b(5) xor s;
+  som5: somcom port map (a => a(5), b => aux, cin => s, s => f(5), cout => carry(5));
+  aux <= b(6) xor s;
+  som6: somcom port map (a => a(6), b => aux, cin => s, s => f(6), cout => carry(6));
+  aux <= b(7) xor s;
+  som7: somcom port map (a => a(7), b => aux, cin => s, s => f(7), cout => carry(7));
+  
 end main;
